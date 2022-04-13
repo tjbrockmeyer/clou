7a995be569a3cf2ef0fc8ccc33abd2003eb67574
Tyler Brockmeyer <tjb101@yahoo.com>
2022-04-12 21:11:39 -0500

    attempt to fix the lightsail user function

--- aws/lambda/lightsail-user/src/index.ts
+++ aws/lambda/lightsail-user/src/index.ts
@@ -1,5 +1,6 @@
 import { readFileSync } from 'fs';
 import { cfnLambda, success, getLightsailConnection, Response } from 'my-utils';
+import SSH2 from 'ssh2-promise'
 
 const schema = JSON.parse(readFileSync('schema.json', 'utf-8'));
 
@@ -12,12 +13,28 @@ interface Props {
 
 interface Data {}
 
-const setCreds = async (props: Props): Promise<Response<Data>> => {
-    const ssh = await getLightsailConnection(props.InstanceName, props.PrivateKey);
-    await ssh?.exec(`\
+const safeExec = async (ssh: SSH2, cmd: string): Promise<string> => {
+    try {
+        return ssh.exec(cmd);
+    } catch(error) {
+        if(error instanceof Buffer) {
+            throw new Error(`command \`${cmd}\` encountered an error: \n${error.toString('utf-8')}`)
+        }
+        throw error;
+    }
+}
+
+const setCreds = async (ssh: SSH2, props: Props): Promise<Response<Data>> => {
+    await safeExec(ssh, `\
 aws configure set aws_access_key_id ${props.AccessKeyId} --profile main && 
 aws configure set aws_secret_access_key ${props.SecretAccessKey} --profile main`);
     return success();
+}
+
+const deleteCreds = async (ssh: SSH2) => {
+    await safeExec(ssh, `\
+aws configure set aws_access_key_id '' --profile main &&
+aws configure set aws_secret_access_key '' --profile main`);
 }
 
 export const handler = cfnLambda<Props, Data>({
@@ -26,13 +43,24 @@ export const handler = cfnLambda<Props, Data>({
         const ssh = await getLightsailConnection(props.InstanceName, props.PrivateKey);
         return ssh !== undefined && await ssh.exec(`aws configure get aws_access_key_id --profile main`) !== '';
     },
-    onCreate: setCreds,
-    onUpdate: setCreds,
+    onCreate: async (props) => {
+        const ssh = await getLightsailConnection(props.InstanceName, props.PrivateKey) as SSH2;
+        await setCreds(ssh, props);
+        return success();
+    },
+    onUpdate: async (props, before) => {
+        const ssh = await getLightsailConnection(props.InstanceName, props.PrivateKey) as SSH2;
+        if(before.InstanceName !== props.InstanceName) {
+            const beforeSsh = await getLightsailConnection(before.InstanceName, before.PrivateKey);
+            if(beforeSsh) {
+                await deleteCreds(beforeSsh);
+            }
+        }
+        await setCreds(ssh, props)
+        return success();
+    },
     onDelete: async (props) => {
-        const ssh = await getLightsailConnection(props.InstanceName, props.PrivateKey);
-        await ssh?.exec(`\
-aws configure set aws_access_key_id '' --profile main &&
-aws configure set aws_secret_access_key '' --profile main`);
+        const ssh = await getLightsailConnection(props.InstanceName, props.PrivateKey) as SSH2;
         return success();
     },
 })
