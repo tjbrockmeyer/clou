fa8967342ade6b1dc9cb9bad0571da2f0e2997e8
Tyler Brockmeyer <tjb101@yahoo.com>
2022-04-12 20:07:38 -0500

    update custom resources to build with infra-deploy

--- aws/lambda/lightsail-docker-role/src/index.ts
+++ aws/lambda/lightsail-docker-role/src/index.ts
@@ -1,6 +1,6 @@
 import aws from 'aws-sdk'
 import { readFileSync } from 'fs';
-import { cfnLambda, success, getLightsailConnection, failure, Response } from 'my-utils';
+import { cfnLambda, success, getLightsailConnection } from 'my-utils';
 import SSH2 from 'ssh2-promise';
 
 const schema = JSON.parse(readFileSync('schema.json', 'utf-8'));
@@ -22,10 +22,6 @@ interface Creds {
 }
 
 const lightsail = new aws.Lightsail();
-
-const connect = async (instanceName: string, privateKey: string) => {
-    return await getLightsailConnection(instanceName, privateKey);
-}
 
 const assumeRole = async (ssh: SSH2, roleArn: string, instanceName: string): Promise<Creds> => {
     try {
@@ -50,30 +46,35 @@ aws configure set source_profile '' --profile=${profileName} &&
 aws configure set role_session_name '' --profile=${profileName}`);
 }
 
-const createOrUpdate = async (props: Props): Promise<Response<Data>> => {
-    const ssh = await connect(props.InstanceName, props.PrivateKey);
-    if (!ssh) {
-        return failure(`could not establish a connection to the lightsail instance '${props.InstanceName}'`);
-    }
-    await assumeRole(ssh, props.RoleArn, props.InstanceName);
-    await putCredentials(ssh, props.InstanceName, props.RoleArn, props.ProfileName);
-    return success();
-}
-
 export const handler = cfnLambda<Props, Data>({
     schema,
-    onCreate: createOrUpdate,
-    onUpdate: createOrUpdate,
+    resourceExists: async (props) => {
+        const ssh = await getLightsailConnection(props.InstanceName, props.PrivateKey);
+        return ssh !== undefined && ssh.exec(`aws configure get role_arn --profile=${props.ProfileName}`)
+    },
+    onCreate: async (props) => {
+        const ssh = await getLightsailConnection(props.InstanceName, props.PrivateKey) as SSH2;
+        await assumeRole(ssh, props.RoleArn, props.InstanceName);
+        await putCredentials(ssh, props.InstanceName, props.RoleArn, props.ProfileName);
+        return success();
+    },
+    onUpdate: async (props, before) => {
+        const ssh = await getLightsailConnection(props.InstanceName, props.PrivateKey) as SSH2;
+        await assumeRole(ssh, props.RoleArn, props.InstanceName);
+
+        if(props.InstanceName !== before.InstanceName) {
+            const beforeSsh = await getLightsailConnection(before.InstanceName, before.PrivateKey) as SSH2;
+            if(beforeSsh) {
+                await deleteCredentials(beforeSsh, before.ProfileName);
+            }
+        } else if(props.ProfileName !== before.ProfileName) {
+            await deleteCredentials(ssh, before.ProfileName);
+        }
+        await putCredentials(ssh, props.InstanceName, props.RoleArn, props.ProfileName);
+        return success();
+    },
     onDelete: async (props) => {
-        try {
-            await lightsail.getInstance({ instanceName: props.InstanceName }).promise();
-        } catch(_error) {
-            return success();
-        }
-        const ssh = await connect(props.InstanceName, props.PrivateKey);
-        if (!ssh) {
-            return failure(`could not establish a connection to the lightsail instance '${props.InstanceName}'`);
-        }
+        const ssh = await getLightsailConnection(props.InstanceName, props.PrivateKey) as SSH2;
         await deleteCredentials(ssh, props.ProfileName);
         return success();
     },
--- aws/lambda/lightsail-user/src/index.ts
+++ aws/lambda/lightsail-user/src/index.ts
@@ -1,7 +1,5 @@
-import aws from 'aws-sdk'
-import path from 'path';
 import { readFileSync } from 'fs';
-import { cfnLambda, success, getLightsailConnection, failure, Response } from 'my-utils';
+import { cfnLambda, success, getLightsailConnection, Response } from 'my-utils';
 
 const schema = JSON.parse(readFileSync('schema.json', 'utf-8'));
 
@@ -16,10 +14,7 @@ interface Data {}
 
 const setCreds = async (props: Props): Promise<Response<Data>> => {
     const ssh = await getLightsailConnection(props.InstanceName, props.PrivateKey);
-    if(!ssh) {
-        return failure(`could not establish a connection to the lightsail instance '${props.InstanceName}'`);
-    }
-    await ssh.exec(`\
+    await ssh?.exec(`\
 aws configure set aws_access_key_id ${props.AccessKeyId} --profile main && 
 aws configure set aws_secret_access_key ${props.SecretAccessKey} --profile main`);
     return success();
@@ -27,15 +22,15 @@ aws configure set aws_secret_access_key ${props.SecretAccessKey} --profile main`
 
 export const handler = cfnLambda<Props, Data>({
     schema,
+    resourceExists: async (props) => {
+        const ssh = await getLightsailConnection(props.InstanceName, props.PrivateKey);
+        return ssh !== undefined && await ssh.exec(`aws configure get aws_access_key_id --profile main`) !== '';
+    },
     onCreate: setCreds,
     onUpdate: setCreds,
     onDelete: async (props) => {
         const ssh = await getLightsailConnection(props.InstanceName, props.PrivateKey);
-        if(!ssh) {
-            console.error(`could not establish a connection to the lightsail instance '${props.InstanceName}'`);
-            return success();
-        }
-        await ssh.exec(`\
+        await ssh?.exec(`\
 aws configure set aws_access_key_id '' --profile main &&
 aws configure set aws_secret_access_key '' --profile main`);
         return success();
